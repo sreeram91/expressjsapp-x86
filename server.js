823ab7acf257df6dae51524ca695299497e0b7df
Sreeram Jayan <62942671+sreeram91@users.noreply.github.com>
2025-03-06 14:22:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from x86 node app");
+  res.send("Hello from x86 node app | Latest Version");
 });
 
 const PORT = process.env.PORT | 4002;
